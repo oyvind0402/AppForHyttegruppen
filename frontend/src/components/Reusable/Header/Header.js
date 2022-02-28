5c9a11716215eb1a8798669d542e950269801b52
oyvind0402 <56302124+oyvind0402@users.noreply.github.com>
2022-02-28 15:05:48 +0100

    Removed bootstrap and removed some code from layout.css that wasnt doing anything.

--- frontend/src/components/Reusable/Header/Header.js
+++ frontend/src/components/Reusable/Header/Header.js
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { Link, useHistory } from "react-router-dom";
 import LoginContext from "../../../LoginContext/login-context";
+import "./Header.css";
 
 const Header = () => {
   const history = useHistory();
@@ -15,58 +16,13 @@ const Header = () => {
   };
 
   return (
-    <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-      <button
-        type="button"
-        className="navbar-toggler"
-        data-toggle="collapse"
-        data-target="#navbarCollapse"
-        aria-controls="navbarCollapse"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div
-        className="collapse navbar-collapse justify-content-between"
-        id="navbarCollapse"
-      >
-        <div className="d-flex flex-row">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">
-                Hyttelotteriet
-              </Link>
-            </li>
-            {loggedIn && (
-              <li className="nav-item">
-                <Link className="nav-link" to="/admin">
-                  Admin
-                </Link>
-              </li>
-            )}
-          </ul>
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          
         </div>
-        <div className="d-flex flex-row justify-content-end">
-          <ul className="navbar-nav mr-auto">
-            {!loggedIn && (
-              <li className="nav-item">
-                <Link className="nav-link" to="/login">
-                  Login
-                </Link>
-              </li>
-            )}
-            {loggedIn && (
-              <li className="nav-item">
-                <button className="btn btn-dark" onClick={logoutHandler}>
-                  Logout
-                </button>
-              </li>
-            )}
-          </ul>
-        </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 };
 
