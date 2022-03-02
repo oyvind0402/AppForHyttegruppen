0187f7117de8a23dd7a8e53459041806527e7c2c
mbaan2 <mbaan2@gmail.com>
2022-03-02 12:36:59 +0100

    FAQ Component functional might be done?

--- frontend/src/components/FAQ/FAQ.js
+++ frontend/src/components/FAQ/FAQ.js
@@ -1,16 +1,53 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 
 import LoginContext from '../../LoginContext/login-context';
+import FAQ_Question from '../Reusable/FAQ_Question/FAQ_Question';
 
 const FAQ = () => {
   const loginContext = useContext(LoginContext);
-
   const loggedIn = loginContext.loggedIn;
 
+  const [FAQElements, setFAQElements] = useState([
+    {
+      question: 'I am a question',
+      anwser: 'i am an anwser',
+      open: false,
+    },
+    {
+      question: 'I am also a question',
+      anwser: 'i am also an anwser',
+      open: false,
+    },
+  ]);
+
+  //If you press one of the questions the one that is already open will close
+  const toggleFAQ = (index) => {
+    setFAQElements(
+      FAQElements.map((FAQ, i) => {
+        if (i === index) {
+          FAQ.open = !FAQ.open;
+        } else {
+          FAQ.open = false;
+        }
+        return FAQ;
+      })
+    );
+  };
+
   return (
-    <section>
+    <>
       <h1>FAQ info :D</h1>
-    </section>
+      <div className="FAQs">
+        {FAQElements.map((faq, index) => (
+          <FAQ_Question
+            key={index}
+            faq={faq}
+            index={index}
+            toggleFAQ={(index) => toggleFAQ(index)}
+          />
+        ))}
+      </div>
+    </>
   );
 };
 
