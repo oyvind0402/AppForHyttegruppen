d81c6297cf2b21389ec2cd585a240f52bd9e10e6
Ana <anafvana@gmail.com>
2022-03-16 16:54:45 +0100

    Minor: changed order of Period fields

--- backend/data/periods.go
+++ backend/data/periods.go
@@ -7,7 +7,7 @@ import (
 type Period struct {
 	Id     int       `json:"id"`
 	Name   string    `json:"name"`
+	Season Season    `json:"season"`
 	Start  time.Time `json:"start"`
 	End    time.Time `json:"end"`
-	Season Season    `json:"season"`
 }
