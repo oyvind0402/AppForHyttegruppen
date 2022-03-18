3804e10756f9bb6bc12bf39c899ef59f162bf145
mbaan2 <mbaan2@gmail.com>
2022-03-18 16:22:01 +0100

    progressbar søknad completed and mobile friendly

--- frontend/src/components/Soknad/Soknad.js
+++ frontend/src/components/Soknad/Soknad.js
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
-
 import LoginContext from '../../LoginContext/login-context';
+import HeroBanner from '../01-Reusable/HeroBanner/HeroBanner';
+import './Soknad.css';
 
 const Soknad = () => {
   const loginContext = useContext(LoginContext);
@@ -8,9 +9,16 @@ const Soknad = () => {
   const loggedIn = loginContext.loggedIn;
 
   return (
-    <section>
-      <h1>Soknad info :D</h1>
-    </section>
+    <>
+      <HeroBanner name="Søknad om hytte" />
+      <div className="progressbar">
+        <p className="progress-step activeProgress step1">1. Info</p>
+        <hr className="progress-line" />
+        <p className="progress-step passedProgress step2">2. Perioder</p>
+        <hr className="progress-line" />
+        <p className="progress-step not-activeProgress step3">3. Hytter</p>
+      </div>
+    </>
   );
 };
 
