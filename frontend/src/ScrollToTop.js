992220d3b31e5fa13c11f2e752fed83462a08dff
mbaan2 <mbaan2@gmail.com>
2022-03-25 14:46:01 +0100

    footer fixed and responsive

--- frontend/src/ScrollToTop.js
+++ frontend/src/ScrollToTop.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 
+//Based on https://stackoverflow.com/questions/36904185/react-router-scroll-to-top-on-every-transition/58156895
 function ScrollToTop({ history }) {
   useEffect(() => {
     const unlisten = history.listen(() => {
