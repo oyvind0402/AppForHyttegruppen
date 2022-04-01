31fbe746dce0ecb5e06c38e008ff30f1c1362414
mbaan2 <mbaan2@gmail.com>
2022-04-01 12:34:14 +0200

    Cabin page basics is setup now i just wait for the get function to work

--- frontend/src/components/Cabin/Cabin.js
+++ frontend/src/components/Cabin/Cabin.js
@@ -6,6 +6,23 @@ const Cabin = () => {
   const loginContext = useContext(LoginContext);
   const loggedIn = loginContext.loggedIn;
 
+  const link = window.location.href;
+  const pageID = link.split('/');
+  console.log(pageID[pageID.length - 1]);
+  getCabin(pageID[pageID.length - 1]);
+
+  async function getCabin() {
+    const response = await fetch('/cabin/get', {
+      method: 'POST',
+      body: JSON.stringify(pageID),
+    });
+
+    const data = await response.json();
+    if (response.ok) {
+      //do something
+    }
+  }
+
   return (
     <>
       <HeroBanner name="Hytte" />
