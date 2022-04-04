50458e3abea8e62d3b325e1def1917b392b9f943
oyvind0402 <56302124+oyvind0402@users.noreply.github.com>
2022-04-04 15:14:41 +0200

    Initial way to show all trips (at the moment it shows all applications since we dont have any applications in the DB that are set to winner=true, but that can easily be changed)

---
+++ frontend/src/components/Admin/TripHistory.js
@@ -0,0 +1,98 @@
+import { useEffect, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
+import HeroBanner from '../01-Reusable/HeroBanner/HeroBanner';
+import './TripHistory.css';
+
+const TripHistory = () => {
+  const [trips, setTrips] = useState([]);
+  let cabins = '';
+
+  const fetchApplications = async () => {
+    const response = await fetch('/application/all', {
+      response: 'GET',
+    });
+
+    const data = await response.json();
+    if (response.ok) {
+      setTrips(data);
+    }
+  };
+
+  function getFormattedDate(inDate) {
+    let date = new Date(inDate);
+    let year = date.getFullYear();
+
+    let month = (1 + date.getMonth()).toString();
+    if (month.length < 2) {
+      month = '0' + month;
+    }
+    let day = date.getDate().toString();
+    if (day.length < 2) {
+      day = '0' + day;
+    }
+    return day + '/' + month + '/' + year;
+  }
+
+  useEffect(() => {
+    fetchApplications();
+  }, []);
+
+  return (
+    <>
+      <HeroBanner name="Alle søknader" />
+      <p className="application-title">Alle søknader ({trips.length})</p>
+      <div className="application-container">
+        {trips.map((item, index) => {
+          cabins = '';
+          return (
+            <div className="application" key={index}>
+              <h2>Søknads ID #{item.applicationId}</h2>
+              <div className="application-wrapper">
+                <div className="application-half">
+                  <h4>Accenture ID:</h4>
+                  <p>{item.accentureId}</p>
+                  <h4>Bruker:</h4>
+                  <p>{item.userId}</p>
+                  <h4>Tilfeldig / Spesifikk hytte:</h4>
+                  <p>
+                    {item.cabinAssignment === 'random'
+                      ? 'Tilfeldig'
+                      : 'Spesifikk'}
+                  </p>
+                  <h4>Valgte hytter:</h4>
+                  <div className="application-cabins-wrapper">
+                    {item.cabins.map((cabin) => {
+                      cabins += cabin.cabinName += ' ';
+                    })}
+                    <p>{cabins}</p>
+                  </div>
+                  <h4>Antall hytter ønsket:</h4>
+                  <p>{item.numberOfCabins}</p>
+                </div>
+                <div className="application-half">
+                  <h4>Periode informasjon:</h4>
+                  <p>ID: {item.period.id}</p>
+                  <p>Navn: {item.period.name}</p>
+                  <p>Sesong: {item.period.season.seasonName}</p>
+                  <p>Startdato: {getFormattedDate(item.period.start)}</p>
+                  <p>Sluttdato: {getFormattedDate(item.period.end)}</p>
+                  <h4>Type tur:</h4>
+                  <p>
+                    {item.tripPurpose === 'private' ? 'Privat' : 'Prosjekt'}
+                  </p>
+                  <h4>Vinner:</h4>
+                  <p> {item.winner.toString() === 'false' ? 'Nei' : 'Ja'}</p>
+                </div>
+              </div>
+              <Link to={'/tur/' + item.applicationId} className="link btn big">
+                Endre søknad
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default TripHistory;
