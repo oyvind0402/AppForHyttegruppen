766103420edcdd726624c329dad7c4453802c3f0
Ana <anafvana@gmail.com>
2022-04-08 18:36:32 +0200

    Merge branch main

--- frontend/src/components/Cabin/Cabin.js
+++ frontend/src/components/Cabin/Cabin.js
@@ -1,6 +1,10 @@
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import LoginContext from '../../LoginContext/login-context';
 import HeroBanner from '../01-Reusable/HeroBanner/HeroBanner';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './Cabin.css';
 
 const Cabin = () => {
   const loginContext = useContext(LoginContext);
@@ -8,25 +12,50 @@ const Cabin = () => {
 
   const link = window.location.href;
   const pageID = link.split('/');
-  console.log(pageID[pageID.length - 1]);
-  getCabin(pageID[pageID.length - 1]);
 
-  async function getCabin() {
-    const response = await fetch('/cabin/get', {
-      method: 'POST',
-      body: JSON.stringify(pageID),
-    });
-
-    const data = await response.json();
-    if (response.ok) {
-      //do something
+  useEffect(() => {
+    async function getCabin(pageId) {
+      console.log(pageId);
+      fetch(`/cabin/${pageId}`, {
+        method: 'GET',
+      })
+        .then((response) => response.json())
+        .then((data) => setCabinData(data))
+        .catch((error) => console.log(error));
     }
-  }
+    getCabin(pageID[pageID.length - 1]);
+  }, []);
+
+  const [cabinData, setCabinData] = useState({});
+  console.log(cabinData);
+
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
 
   return (
     <>
-      <HeroBanner name="Hytte" />
-      <h1>Cabin info :D</h1>
+      <HeroBanner name={cabinData.name} />
+      <div className="cabin-display">
+        <Slider {...settings}>
+          {typeof cabinData !== null &&
+            cabinData.pictures !== undefined &&
+            cabinData.pictures.otherPictures.map((file) => {
+              return (
+                <div className="slick-slide-item">
+                  <img
+                    src={`${process.env.PUBLIC_URL}/assets/pictures/${file.filename}`}
+                    alt={file.filename}
+                  />
+                </div>
+              );
+            })}
+        </Slider>
+      </div>
     </>
   );
 };
