8f788d6363ff73ea3e03a811d89337fada6763fc
Ana <anafvana@gmail.com>
2022-04-20 17:10:21 +0200

    Setting hytteroot env variable

--- startup/hytteapi/main.go
+++ startup/hytteapi/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "bachelorprosjekt/backend/server"
+import (
+	"log"
+	"os"
+	"regexp"
+	"strings"
+
+	"bachelorprosjekt/backend/server"
+)
 
 func main() {
+	setRoot()
+	print(os.Getenv("hytteroot"))
 	server.Start()
+}
+
+func setRoot() {
+	// Get working directory
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Remove all content after "AppForHyttegruppen" (/ for Unix, \ for Windows)
+	re := regexp.MustCompile(`(?m)^(.*AppForHyttegruppen[/\\]).*`)
+	root := re.ReplaceAllString(wd, "${1}")
+	root = strings.TrimSuffix(root, "/")
+	root = strings.TrimSuffix(root, "\\")
+
+	os.Setenv("hytteroot", root)
 }
