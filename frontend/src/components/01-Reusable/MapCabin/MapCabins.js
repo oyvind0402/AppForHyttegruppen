47b4a143a9d46cd8bcd65a60433d91014f866674
mbaan2 <mbaan2@gmail.com>
2022-04-22 15:19:43 +0200

    map fix

--- frontend/src/components/01-Reusable/MapCabin/MapCabins.js
+++ frontend/src/components/01-Reusable/MapCabin/MapCabins.js
@@ -21,7 +21,7 @@ const MapCabins = (props) => {
         XPathResult.FIRST_ORDERED_NODE_TYPE,
         null
       ).singleNodeValue;
-      if (map !== null) map.style.width = '80%';
+      if (typeof map !== undefined) map.style.width = '80%';
     }
 
     window.addEventListener('resize', handleResize);
