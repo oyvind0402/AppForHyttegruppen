2a84976d89f635bd446d0a60d364d361252e3b7c
mbaan2 <mbaan2@gmail.com>
2022-05-11 13:40:41 +0200

    found a wrong request and fixed it

--- frontend/src/components/Soknad/Steps/Step1.js
+++ frontend/src/components/Soknad/Steps/Step1.js
@@ -21,7 +21,7 @@ const Step1 = (props) => {
   //Fetching
   useEffect(() => {
     async function fetchData() {
-      fetch('/cabin/all')
+      fetch('/cabin/active')
         .then((response) => response.json())
         .then((data) => setCabins(data))
         .catch((error) => console.log(error));
