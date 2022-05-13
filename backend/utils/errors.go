61034ef134f3a29b30ab292953a7818308efcb97
Ana <anafvana@gmail.com>
2022-05-13 16:41:54 +0200

    Removed unused AbortWithStatus()

--- backend/utils/errors.go
+++ backend/utils/errors.go
@@ -2,19 +2,10 @@ package utils
 
 import (
 	"fmt"
-	"net/http"
-
-	"github.com/gin-gonic/gin"
 )
 
 func Panicker(err error, msg string) {
 	if err != nil {
 		panic(fmt.Sprintf("%s. ERROR:\n%s", msg, err.Error()))
-	}
-}
-
-func AbortWithStatus(err error, ctx gin.Context) {
-	if err != nil {
-		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"err": err.Error()})
 	}
 }
